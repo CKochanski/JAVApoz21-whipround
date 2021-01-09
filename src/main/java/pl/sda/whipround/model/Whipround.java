34fda79f5b93c8bce946f775f11752a89ebc61c8
Wojciech Łakomy <lakomywojtek@gmail.com>
2021-01-09 15:03:50 +0100

    add new model

---
+++ src/main/java/pl/sda/whipround/model/Whipround.java
@@ -0,0 +1,27 @@
+package pl.sda.whipround.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Entity
+public class Whipround {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private BigDecimal goal;
+    private String description;
+    @OneToMany
+    private List<Donation> donations;
+
+}
