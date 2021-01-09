ed2d4a942791c271b2c659309cd6f888d5cca5f0
Cezary Kochański <cezarykochanski@Air-Cezary.wtvk.pl>
2021-01-09 13:58:52 +0100

    Initial commit

---
+++ src/main/java/pl/sda/whipround/Javapoz21WhiproundApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.whipround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javapoz21WhiproundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javapoz21WhiproundApplication.class, args);
+	}
+
+}
